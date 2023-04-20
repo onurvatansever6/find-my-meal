fc3099dcd1ee420c9a37c1c2b314d50bff3a6f28
onurvatansever6 <onurvatansever.6@gmail.com>
2023-04-20 13:00:57 +0300

    Remaining recipes added to src

--- bin/RecipeScraper.js
+++ bin/RecipeScraper.js
@@ -6,27 +6,33 @@ const links = JSON.parse(fs.readFileSync('Links.json'));
 console.log(links.length);
 let recipes = [];
 
-let recipe = {
-    name: "",
-    quantityIngredients: "",
-    servings: "",
-    prepTime: "",
-    directions: "",
-    ingredients: {
-        keywords: [],
-        fullDescription: []
-    },
-};
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://www.google.com/',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+  };
 
-async function scrape(index) {
-    const response = await axios.get(links[index]);
+
+  async function scrape(index) {
+    const response = await axios.get(links[index], { headers });
     const $ = cheerio.load(response.data);
 
-    recipe.name = $('.svelte-1muv3s8').text();
-    recipe.prepTime = $('dd.facts__value').eq(0).text().trim();
-    recipe.quantityIngredients = $('dd.facts__value').eq(1).text().trim();
-    recipe.servings = $('dd.facts__value').eq(2).text().trim();
-    recipe.directions = $('.direction.svelte-5aqxnx').map((_, el) => $(el).text().trim()).get();
+    const recipe = {
+        name: $('.svelte-1muv3s8').text(),
+        prepTime: $('dd.facts__value').eq(0).text().trim(),
+        quantityIngredients: $('dd.facts__value').eq(1).text().trim(),
+        servings: $('dd.facts__value').eq(2).text().trim(),
+        directions: $('.direction.svelte-ovaflp').map((_, el) => $(el).text().trim()).get(),
+        ingredients: {
+            keywords: [],
+            fullDescription: []
+        },
+    };
 
     $('.ingredient-list li').each((i, el) => {
         const quantity = $(el).find('.ingredient-quantity').text().trim();
@@ -46,37 +52,32 @@ async function scrape(index) {
         }
     });
 
-    recipes.push(recipe); 
-    recipe = {
-        name: "",
-        quantityIngredients: "",
-        servings: "",
-        prepTime: "",
-        directions: "",
-        ingredients: {
-            keywords: [],
-            fullDescription: []
-        },
-    };
-    console.log(recipes[index])
+    return recipe;
 }
+
 
 
 let promises = [];
 
-for (let i = 0; i < 40; i++) {
+for (let i = 0; i < links.length; i++) {
     let promise = new Promise((resolve) => {
         setTimeout(() => {
-            scrape(i);
-            resolve();
+            scrape(i).then((recipe) => {
+                recipes.push(recipe);
+                console.log(recipe);
+                resolve();
+            });
         }, i * 500);
     });
     promises.push(promise);
+    
+    if ((i + 1) % 100 === 0 || i === links.length - 1) {
+        Promise.all(promises).then(() => {
+            fs.writeFileSync('Recipes.json', JSON.stringify(recipes), { flag: 'a' });
+            console.log(`Data written to file for recipes ${i - 99} to ${i}`);
+            recipes = [];
+        });
+        promises = [];
+    }
 }
 
-Promise.all(promises).then(() => {
-    fs.writeFile('Recipes.json', JSON.stringify(recipes), (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
-    });
-});
