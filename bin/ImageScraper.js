499517b7db77ffaa9f8e7f7aa2e750829988b19a
Onur Vatansever <onurvatansever.6@gmail.com>
2023-05-05 17:31:32 +0300

    ImageScraper modified

--- bin/ImageScraper.js
+++ bin/ImageScraper.js
@@ -3,13 +3,37 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 const links = JSON.parse(fs.readFileSync('../src/Links.json'));
+const imageLinks = [];
+
+const headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Referer': 'https://www.google.com/',
+    'DNT': '1',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+  };
+
 
 async function scrape() {
-    const response = await axios.get(links[0]);
-    const $ = cheerio.load(response.data);
+    for (const link of links) {
+        const response = await axios.get(link, { headers });
+        const $ = cheerio.load(response.data);
 
-    const imageLink = $('.svelte-kb6fq').attr('src');
-    console.log(imageLink);
+        const imageLink = $('.svelte-wgcq7z img').attr('srcset').split(" ");
+        imageLinks.push(imageLink[0]);
+        console.log("scraping links...");
+
+        // Add a delay of 1 second between requests
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    fs.writeFile('../src/Images.json', JSON.stringify(imageLinks), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
 }
 
 scrape();
